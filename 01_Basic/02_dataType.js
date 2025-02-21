569b92f6cba588ea60984357eac9bfafb540d150
Yash Pawar <112814699+Yash-Pawar-1410@users.noreply.github.com>
2025-02-21 16:03:07 +0000

    datatype

---
+++ 01_Basic/02_dataType.js
@@ -0,0 +1,13 @@
+let employeeCode = 3698
+let employeeName = "Yash"
+let employeePresent = false
+let employeeAddress = null
+let employeeExperience = undefined
+
+console.log(typeof employeeCode);
+console.log(typeof employeeName);
+console.log(typeof employeePresent);
+console.log(typeof employeeAddress);
+console.log(typeof employeeExperience);
+
+
