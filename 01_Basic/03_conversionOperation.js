93d50272f023749862590034d22cd6f0312c40d8
Yash Pawar <112814699+Yash-Pawar-1410@users.noreply.github.com>
2025-02-21 16:19:35 +0000

    Conversion Operation

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,52 @@
+let runs = 225
+let runs1= "Raje"
+let runs2 = "aa12"
+let runs3 = null
+let runs4 = false
+let runs5 = undefined
+
+console.log(runs);
+console.log(typeof runs);
+console.log("-------------");
+
+console.log(runs1);
+console.log(typeof runs1);
+let conversion = Number (runs1)
+console.log(conversion);
+console.log(typeof conversion);
+console.log("-------------");
+
+console.log(runs2);
+console.log(typeof runs2);
+let conversion2 = Number (runs2)
+console.log(conversion2);
+console.log(typeof conversion2);
+console.log("-------------");
+
+console.log(runs3);
+console.log(typeof runs3);
+let conversion3 = Number (runs3)
+console.log(conversion3);
+console.log(typeof conversion3);
+console.log("-------------");
+
+console.log(runs4);
+console.log(typeof runs4);
+let conversion4 = Number (runs4)
+console.log(conversion4);
+console.log(typeof conversion4);
+console.log("-------------");
+
+console.log(runs5);
+console.log(typeof runs5);
+let conversion5 = Number (runs5)
+console.log(conversion5);
+console.log(typeof conversion5);
+console.log("-------------");
+
+// console.log(runs5);
+// console.log(typeof runs5);
+// let conversion6 = null (runs5)
+// console.log(conversion6);
+// console.log(typeof conversion6);
+// console.log("-------------");
